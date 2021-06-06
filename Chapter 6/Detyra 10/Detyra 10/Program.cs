ff9fd3cfd9be826a76e681140caa1e094d91ba00
GWKwinBM <pg49240@universum-ks.org>
2021-06-06 11:42:33 +0200

    Detyrat 7-12

--- Chapter 6/Detyra 10/Detyra 10/Program.cs
+++ Chapter 6/Detyra 10/Detyra 10/Program.cs
@@ -4,18 +4,19 @@ namespace Detyra_10
 {
     class Program
     {
+        private const int V = 3;
+
         static void Main(string[] args)
         {
-			Console.WriteLine("Matrix pattern");
+            Console.WriteLine("The Matrix pattern");
+            for (int row = 0; row < 3; row++)
+                for (int column = row; column + V; column++)
+                {
+                    Console.WriteLine($"{column + 1,3}");
+                }
+            Console.WriteLine();
 
-			for (int row = 0; row < 3; row++)
-			{
-				for (int column = row; column < row + 3; column++)
-				{
-					Console.Write($"{column + 1,3}");
-				}
-				Console.WriteLine();
-			}
-		}
+
+        }
     }
 }
